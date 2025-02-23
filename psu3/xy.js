c3c82ef58ec2711751da0a412bec0c26a55c6561
NatthapongTelakul <6510210155@psu.ac.th>
2025-02-23 21:07:51 +0700

    Delete psu3 directory

--- psu3/xy.js
+++
@@ -1,6 +0,0 @@
-function calculate(){
-    const vx = document.getElementById("x").value;
-    const vy = document.getElementById("y").value;
-    const vresult = document.getElementById("result");
-    vresult.value = parseFloat(vx) + parseFloat(vy);
-}
